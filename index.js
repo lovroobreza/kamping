76297e208e9a6eba083c6eeb6748d58bd56de604
Lovro <obreza.lovro@gmail.com>
2021-08-01 11:24:51 +0200

    done with the crud, now styles

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+
+//midleware
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use(methodOverride('_method'))
 
 //mongoose
 mongoose.connect('mongodb://localhost:27017/camp', {
@@ -35,6 +42,11 @@ app.get('/campgrounds', async(req,res)=>{
     res.render('campgrounds/index', {campgrounds})
 })
 
+app.post('/campgrounds', async(req,res)=>{
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.get('/campgrounds/new', (req,res)=>{
     res.render('campgrounds/new')
@@ -45,6 +57,25 @@ app.get('/campgrounds/:id', async(req,res)=>{
     const campground = await Campground.findById(id)
     res.render('campgrounds/show', {campground})
 })
+
+app.get('/campgrounds/:id/edit', async(req,res)=>{
+    const {id} = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', {campground})
+})
+
+app.put('/campgrounds/:id', async (req,res)=>{
+    const {id}= req.params
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async(req,res)=>{
+    const {id}= req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
+
 //port
 app.listen(3000,()=>{
     console.log('listening on port 3k')
