a27c202f061676584b4603cf233d422379d706f7
Lovro <obreza.lovro@gmail.com>
2021-08-01 14:11:10 +0200

    design work

--- seeds/index.js
+++ seeds/index.js
@@ -27,10 +27,13 @@ const seedDb = async() =>{
     await Campground.deleteMany({})
     for(let i = 0; i<50; i++){
         const randomCity = Math.floor(Math.random()*1000)
-
+        const price = Math.floor(Math.random()*50)
         const c = new Campground({
+            title: `${sample(descriptors)} ${sample(places)}`,
             location:`${cities[randomCity].city}, ${cities[randomCity].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like A',
+            price
         })
 
         await c.save()
